e59420ae4ce092dd4ecea82f3ec87131711ea5bb
Daniel Hansson <github@hanssonit.se>
2020-07-08 23:33:41 +0200

    Update prep_vm.sh

--- prep_vm.sh
+++ prep_vm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
+
+####### MASTER #######
+
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/official/lib.sh)
-
-# T&M Hansson IT AB © - 2020, https://www.hanssonit.se/
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/masterl/lib.sh)
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
@@ -13,27 +15,22 @@ true
 DEBUG=0
 debug_mode
 
-# Check if dpkg or apt is running
-is_process_running apt
-is_process_running dpkg
-
 # Create scripts folder
 mkdir -p "$SCRIPTS"
 
 # Get needed scripts for first bootup
-download_script STATIC instruction
-download_script STATIC history
-download_script STATIC static_ip
 download_script GITHUB_REPO lib
 download_script GITHUB_REPO nextcloud_install_production
+download_script STATIC history
+download_script STATIC static_ip
 
 # Make $SCRIPTS excutable
 chmod +x -R "$SCRIPTS"
 chown root:root -R "$SCRIPTS"
 
-# Prepare first bootup
-check_command run_script STATIC change-ncadmin-profile
-check_command run_script STATIC change-root-profile
+# Check if dpkg or apt is running
+is_process_running apt
+is_process_running dpkg
 
 # Upgrade
 apt update -q4 & spinner_loading
@@ -49,3 +46,17 @@ then
     echo "USER: ncadmin" >> /etc/issue
     echo "PASS: nextcloud" >> /etc/issue
 fi
+
+####### OFFICIAL (custom scripts) #######
+
+# shellcheck disable=2034,2059
+true
+# shellcheck source=lib.sh
+. <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/official/lib.sh)
+
+# Get needed scripts for first bootup
+download_script STATIC instruction
+
+# Prepare first bootup
+check_command run_script STATIC change-ncadmin-profile
+check_command run_script STATIC change-root-profile
